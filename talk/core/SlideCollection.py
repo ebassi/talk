34d7144700b227489fddb3cfb781f964f9d45ba5
Emmanuele Bassi <ebassi@openedhand.com>
2008-02-14 18:02:10 +0000

    Wrap more collection methods
    A SlideCollection should behave like a collection from the outside
    world. Since the zero-eth slide is the title slide it should also be
    special cased so that only *one* title slide is present in a collection.

--- talk/core/SlideCollection.py
+++ talk/core/SlideCollection.py
@@ -5,6 +5,10 @@ from talk.core.TitleSlide import TitleSlide
 
 class SlideCollection:
     """
+    A collection of slides
+
+    Iterable container. The title slide can only be one, and of
+    TitleSlide type.
     """
     def __init__ (self):
         self.slides = []
@@ -14,16 +18,40 @@ class SlideCollection:
         if not isinstance(slide, TitleSlide):
             raise TypeError('slide must be a TitleSlide')
         else:
+            if self.slides and self.slides[0] is TitleSlide:
+                self.slides[0] = None
+
             self.slides.insert(0, slide)
 
     def get_title_slide (self):
-        return self.slides[0]
+        if self.slides and self.slides[0] is TitleSlide:
+            return self.slides[0]
+        else:
+            return None
 
     def append_slide (self, slide):
         if not isinstance(slide, TalkSlide):
             raise TypeError('slide must be a TalkSlide')
         else:
             self.slides.append(slide)
+
+    def append (self, slide):
+        if slide is TitleSlide:
+            self.set_title_slide(slide)
+        else:
+            self.append_slide(slide)
+
+    def get_slide (self, index):
+        return self.slides[index]
+
+    def __getitem__ (self, key):
+        return self.slides[key]
+
+    def __setitem__ (self, key, value):
+        if key == 0:
+            self.set_title_slide(value)
+        else:
+            self.slides[key] = value
 
     def __len__ (self):
         return len(self.slides)
@@ -39,5 +67,4 @@ class SlideCollection:
         self.cur_slide = self.cur_slide + 1
 
         return item
-
 
