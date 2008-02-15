6e7c8dfc525b4d4e6da3221b7996e5de0c41203b
Emmanuele Bassi <ebassi@openedhand.com>
2008-02-15 00:19:39 +0000

    Add GObject properties to TextSlide
    If we want to be able to set the text and title of TextSlide using
    ClutterScript those python-only members must be accessible through
    the usual GObject properties thing-a-magig.

--- talk/core/TextSlide.py
+++ talk/core/TextSlide.py
@@ -1,12 +1,17 @@
 import gobject
+import pango
 import clutter
 
 from talk.core.TalkSlide import TalkSlide
 
 class TextSlide (TalkSlide):
     __gtype_name__ = 'TextSlide'
+    __gproperties__ = {
+        'title' : (str, 'title', 'title', '', gobject.PARAM_READWRITE),
+        'text' : (str, 'text', 'text', '', gobject.PARAM_READWRITE),
+    }
 
-    def __init__ (self, title=None, text=None):
+    def __init__ (self, title=None, text=None, **kwargs):
         header = None
         if kwargs.has_key('header'):
             header = kwargs['header']
@@ -25,30 +30,53 @@ class TextSlide (TalkSlide):
 
         TalkSlide.__init__(self, header, footer, bg_color, text_color)
 
-        self.title = title
-        if self.title:
-            self._title_label = clutter.Label()
-            self._title_label.set_font_name('Sans 36px')
-            self._title_label.set_use_markup(True)
-            self._title_label.set_text(self.title)
-            self._title_label.set_width(600)
-            self.add(self._title_label)
+        self._title = title
+        self._title_label = clutter.Label()
+        self._title_label.set_color(self.get_text_color())
+        self._title_label.set_font_name('Sans 36px')
+        self._title_label.set_use_markup(True)
+        if self._title:
+            self._title_label.set_text(self._title)
+        self._title_label.set_width(600)
+        self.add(self._title_label)
 
-            x = 50
-            y = self.get_header_width() + 50
-            self._title_label.set_position(x, y)
-            self._title_label.show()
+        x = 50
+        y = self.get_header_width() + 50
+        self._title_label.set_position(x, y)
+        self._title_label.show()
 
-        self.text = text
-        if self.text:
-            self._text_label = clutter.Label()
-            self._text_label.set_font_name('Sans 28px')
-            self._text_label.set_use_markup(True)
-            self._text_label.set_text(self.text)
-            self._text_label.set_width(600)
-            self.add(self._text_label)
+        self._text = text
+        self._text_label = clutter.Label()
+        self._text_label.set_color(self.get_text_color())
+        self._text_label.set_font_name('Sans 28px')
+        self._text_label.set_use_markup(True)
+        if self._text:
+            self._text_label.set_text(self._text)
+        self._text_label.set_width(600)
+        self.add(self._text_label)
 
-            x = 50
-            y = self.get_header_width() + 100
-            self._text_label.set_position(x, y)
-            self._text_label.show()
+        x = (800 - self._text_label.get_width()) / 2
+        y = (600 - self._text_label.get_height()) / 2
+        self._text_label.set_alignment(pango.ALIGN_CENTER)
+        self._text_label.set_position(x, y)
+        self._text_label.show()
+
+    def do_set_property (self, pspec, value):
+        if pspec.name == 'title':
+            self._title = value
+            self._title_label.set_text(self._title)
+        elif pspec.name == 'text':
+            self._text = value
+            self._text_label.set_text(self._text)
+        else:
+            raise TypeError('Unknown property ' + pspec.name)
+
+    def do_get_property (self, pspec):
+        if pspec.name == 'title':
+            return self._title
+        elif pspec.name == 'text':
+            return self._text
+        else:
+            raise TypeError('Unknown property ' + pspec.name)
+
+gobject.type_register(TextSlide)
